27fd17b2fdc599805310c41dff539022f9d78020
TheSecretUndergroundCoder <135362041+TheSecretUndergroundCoder@users.noreply.github.com>
2024-08-29 21:10:54 +0100

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,1 +1,17 @@
+// Modal Logic
+const imageModal = document.getElementById('imageModal');
+const modalImage = document.getElementById('modalImage');
+const imageCaption = document.getElementById('imageCaption');
+
+// Open Modal
+function openModal(url, title) {
+    imageModal.classList.add('show');
+    modalImage.src = url;
+    imageCaption.textContent = title;
+}
+
+// Close Modal
+imageModal.querySelector('.close').addEventListener('click', () => {
+    imageModal.classList.remove('show');
+});
 
